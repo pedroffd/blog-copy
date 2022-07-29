7064b20759c4c998ea525aa4504ed5dc5a5aa136
pedroffd <pedro.alcarin@gmail.com>
2022-07-28 21:33:34 -0300

    style for searchbox component

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,6 +1,7 @@
-import { BsSearch } from 'react-icons/bs';
+import { SearchBoxComponent } from '@/components/search/SearchBox';
 
 import { Meta } from '../layout/Meta';
+
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
@@ -12,23 +13,7 @@ const Base = () => (
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <TopBar />
     <Banner />
-    <div className="flex flex-row justify-center mx-auto relative bg-gray-800 p-2 rounded-[24px] border-2 border-gray-400 h-20 w-6/12 shadow-lg shadow-gray-500/50">
-      <div className="md:h-full">
-        <BsSearch
-          style={{
-            color: 'gray',
-            fontSize: '24px',
-            marginLeft: '8px',
-            verticalAlign: 'baseline',
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full p-4 ml-6 placeholder-gray-600 bg-transparent"
-        ></input>
-      </div>
-    </div>
+    <SearchBoxComponent />
     <VerticalFeatures />
     <Footer />
   </div>
