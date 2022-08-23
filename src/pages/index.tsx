23922b0952f95ff63196f6cc51883e265c071910
pedroffd <pedro.alcarin@gmail.com>
2022-08-23 17:36:44 -0300

    require auth middleware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { useSession } from 'next-auth/react';
-
 import { Base } from '../templates/Base';
 
 const Index = () => {
-  const { data: session } = useSession();
-
-  return <Base sessionData={session} />;
+  return <Base />;
 };
 
 export default Index;
