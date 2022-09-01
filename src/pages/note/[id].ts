1ca6f8e98ce2a6cba99ae1fa5ed386380292d3eb
pedroffd <pedro.alcarin@gmail.com>
2022-09-01 01:22:00 -0300

    basic crud finished to be served as template

---
+++ src/pages/note/[id].ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import prisma from '@/../../lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const noteId = req.query.id;
+
+  if (req.method === 'DELETE') {
+    const note = await prisma.note.delete({
+      where: { id: Number(noteId) },
+    });
+    res.json(note);
+  } else {
+    res.status(400).send({ message: 'error' });
+  }
+}
